01c4190c69bd02f857e4aea7b49022b67f6710d8
Cal Henderson <cal@iamcal.com>
2012-09-10 13:42:38 -0700

    added french translation by Thierry/VDV

---
+++ Locales/Strings-frFR.lua
@@ -0,0 +1,81 @@
+
+BuffGuideLocales["frFR"] = {
+
+	ADDON_NAME	= "BuffGuide",
+
+	RAID_BUFFS	= "Buffs de raid",
+	FOOD_BUFF	= "Buff nourriture",
+	FLASK		= "Flacon",
+	YES		= "Oui",
+	MISSING		= "Manquant",
+
+	-- this next string is required to make flasks work
+	FLASK_OF	= "Flacon de",
+
+	TIP_CLICK = "|cff7fff7fClick|r to toggle the BuffGuide window.",
+
+	LDB_NO_FOOD		= "No food",
+	LDB_NO_FLASK		= "No flask",
+	LDB_NO_FOOD_FLASK	= "No food or flask",
+
+	TIME_S		= "%ss",
+	TIME_M		= "%sm",
+	TIME_HM		= "%sh %sm",
+
+	BUFF_stats		= "Carac.",
+	BUFF_stamina		= "Endurance",
+	BUFF_attack_power	= "Puissance d'attaque",
+	BUFF_spell_power	= "Puissance des sorts",
+	BUFF_haste		= "Hâte",
+	BUFF_spell_haste	= "Hâte des sorts",
+	BUFF_crit		= "Critique",
+	BUFF_mastery		= "Maîtrise",
+
+	FROM_DRUID	= "Druide",
+	FROM_MONK	= "Moine",
+	FROM_PALADIN	= "Paladin",
+	FROM_DK		= "Chevalier de la mort",
+	FROM_HUNTER	= "Chasseur",
+	FROM_MAGE	= "Mage",
+	FROM_SHAMAN	= "Chaman",
+	FROM_PRIEST	= "Prêtre",
+	FROM_WARRIOR	= "Guerrier",
+	FROM_WARLOCK	= "Démoniste",
+	FROM_ROGUE	= "Voleur",
+
+	FROM_IMP	= "Démoniste (Diablotin)",
+	FROM_DK_FU	= "DK Givre/Impie",
+	FROM_SHAM_ENC	= "Chaman Amélioration",
+	FROM_SHAM_ELE	= "Chaman Élémentaire",
+	FROM_DRUID_BAL	= "Druide Équilibre",
+	FROM_DRUID_FER	= "Druide Gardien/Farouche",
+	FROM_SPRIEST	= "Prêtre Ombre",
+
+	FROM_SHALE	= "Chasseur MB (Araignée de schiste)",
+	FROM_SILITHID	= "Chasseur MB (Silithide)",
+	FROM_WSTRIDER	= "Chasseur MB (Trotteur Aquatique)",
+	FROM_HYENA	= "Chasseur (Hyène)",
+	FROM_SERPENT	= "Chasseur (Serpent)",
+	FROM_HYDRA	= "Chasseur (Hydre)",
+	FROM_WOLF	= "Chasseur (Loup)",
+	FROM_DEVILSAUR	= "Chasseur (Diablosaure)",
+	FROM_QUILEN	= "Chasseur (Quilen)",
+	FROM_CAT	= "Chasseur (Chat)",
+	FROM_SPIRIT	= "Chasseur (Esprit de bête)",
+
+	SLASH_COMMAND	= "/buffguide",
+	SLASH_HELP = "BuffGuide commands:",
+
+	SLASH_SHOW	= "show",
+	SLASH_HIDE	= "hide",
+	SLASH_TOGGLE	= "toggle",
+	SLASH_RESET	= "reset",
+
+	SLASH_SHOW_HELP = "Show frame",
+	SLASH_HIDE_HELP = "Hide frame",
+	SLASH_TOGGLE_HELP = "Toggle frame",
+	SLASH_RESET_HELP = "Reset frame position and size",
+
+	MENU_HIDE	= "Hide frame",
+	MENU_HIDE_TIP	= "BuffGuide has been hidden. Type \"%s\" to show it again.",
+};
