8e4a29ff715b4d3f2469efbde34651cb8073d965
Cal Henderson <cal@iamcal.com>
2012-09-11 09:50:59 -0700

    added chinese translation from laxgenius

---
+++ Locales/Strings-zhTW.lua
@@ -0,0 +1,84 @@
+
+BuffGuideLocales["zhTW"] = {
+
+	ADDON_NAME	= "Buff指南",
+
+	RAID_BUFFS	= "團隊 Buff",
+	FOOD_BUFF	= "食物",
+	FLASK		= "精鍊",
+	YES		= "已有",
+	MISSING		= "缺少",
+
+	-- this next string is required to make flasks work
+	FLASK_OF	= "精鍊",
+
+	TIP_CLICK = "|cff7fff7f點擊|r 切換Buff指南的視窗。",
+
+	LDB_NO_FOOD		= "缺少食物",
+	LDB_NO_FLASK		= "缺少精鍊",
+	LDB_NO_FOOD_FLASK	= "缺少食物或精鍊",
+
+	TIME_S		= "%ss",
+	TIME_M		= "%sm",
+	TIME_HM		= "%sh %sm",
+
+	BUFF_stats		= "屬性",
+	BUFF_stamina		= "耐力",
+	BUFF_attack_power	= "攻擊強度",
+	BUFF_spell_power	= "法術能量",
+	BUFF_haste		= "加速",
+	BUFF_spell_haste	= "法術加速",
+	BUFF_crit		= "爆擊",
+	BUFF_mastery		= "精通",
+
+	FROM_DRUID	= "德魯伊",
+	FROM_MONK	= "武僧",
+	FROM_PALADIN	= "聖騎士",
+	FROM_DK		= "死亡騎士",
+	FROM_HUNTER	= "獵人",
+	FROM_MAGE	= "法師",
+	FROM_SHAMAN	= "薩滿",
+	FROM_PRIEST	= "牧師",
+	FROM_WARRIOR	= "戰士",
+	FROM_WARLOCK	= "術士",
+	FROM_ROGUE	= "盜賊",
+
+	FROM_IMP	= "術士（小鬼）",
+	FROM_DK_FU	= "冰霜/穢邪死騎",
+	FROM_SHAM_ENC	= "增強薩",
+	FROM_SHAM_ELE	= "元素薩",
+	FROM_DRUID_BAL	= "鳥D",
+	FROM_DRUID_FER	= "熊/貓D",
+	FROM_SPRIEST	= "暗牧",
+	FROM_MONK_WW	= "御風武僧",
+
+	FROM_SHALE	= "獸王獵(岩蛛)",
+	FROM_SILITHID	= "獸王獵(異種蟲族)",
+	FROM_WSTRIDER	= "獸王獵(水黽)",
+	FROM_DEVILSAUR	= "獸王獵(魔暴龍)",
+	FROM_QUILEN	= "獸王獵(麒麟獸)",
+	FROM_SPIRIT	= "獸王獵(靈獸)",
+
+	FROM_HYENA	= "獵人(土狼)",
+	FROM_SERPENT	= "獵人(蛇)",
+	FROM_HYDRA	= "獵人(多頭蛇)",
+	FROM_WOLF	= "獵人(狼)",
+	FROM_CAT	= "獵人(貓)",
+
+
+	SLASH_COMMAND	= "/bg",
+	SLASH_HELP = "Buff指南指令:",
+
+	SLASH_SHOW	= "show",
+	SLASH_HIDE	= "hide",
+	SLASH_TOGGLE	= "toggle",
+	SLASH_RESET	= "reset",
+
+	SLASH_SHOW_HELP = "顯示框架",
+	SLASH_HIDE_HELP = "隱藏框架",
+	SLASH_TOGGLE_HELP = "切換框架",
+	SLASH_RESET_HELP = "重置框架位置及大小",
+
+	MENU_HIDE	= "隱藏框架",
+	MENU_HIDE_TIP	= "Buff指南已經隱藏， 輸入 \"%s\" 再次顯示。",
+};
