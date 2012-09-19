87e90730adf7e964195af06228ed79343090c513
Cal Henderson <cal@iamcal.com>
2012-09-19 11:16:54 -0700

    Corrected spell ID for Moonkin Aura (thanks to wenyicchen)

--- BuffGuide.lua
+++ BuffGuide.lua
@@ -77,7 +77,7 @@ BuffGuide.buffs = {
 	},
 	spell_haste = {
 		buffs = {
-			{24858,		L.FROM_DRUID_BAL},	-- Moonkin Aura
+			{24907,		L.FROM_DRUID_BAL},	-- Moonkin Aura
 			{49868,		L.FROM_SPRIEST},	-- Mind Quickening
 			{51470,		L.FROM_SHAM_ELE},	-- Elemental Oath
 		},
